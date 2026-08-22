468d5aa464f90e454fabe4ee4b6521555aaab2ce
Anya-Louni <a.lizz.louni@gmail.com>
2026-08-22 20:28:19 +0100

    Update today.py

--- today.py
+++ today.py
@@ -84,9 +84,7 @@ def graph_repos_stars(count_type, owner_affiliation, cursor=None, add_loc=0, del
                     node {
                         ... on Repository {
                             nameWithOwner
-                            stargazers {
-                                totalCount
-                            }
+                            stargazerCount
                         }
                     }
                 }
@@ -308,11 +306,11 @@ def force_close_file(data, cache_comment):
 
 
 def stars_counter(data):
-    """
-    Count total stars in repositories owned by me
-    """
     total_stars = 0
-    for node in data: total_stars += node['node']['stargazers']['totalCount']
+    for node in data:
+        repository = node.get('node')
+        if repository is not None:
+            total_stars += repository.get('stargazerCount', 0) or 0
     return total_stars
 
 
